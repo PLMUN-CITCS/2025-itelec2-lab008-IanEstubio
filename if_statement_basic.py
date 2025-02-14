4db62177bdfb8496bebb081c7d13d510d594d78d
IanEstubio <kazuhajean27@gmail.com>
2025-02-14 16:11:08 +0800

    Submitting Python Week 04 - Laboratory # 08

---
+++ if_statement_basic.py
@@ -0,0 +1,8 @@
+#IAN JAY P. ESTUBIO 
+#Week 04 - Conditional Statements
+#Laboratory # 08 - Guided Coding Exercise: Simple if Statement in Python
+
+temperature = 35
+
+if temperature > 30:
+     print("It's a hot day!")
